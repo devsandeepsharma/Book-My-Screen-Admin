cdba002d9132e02a4cfc65c885a06530e4355f51
Sandeep <sandipsharma2445@gmail.com>
2025-05-20 21:08:15 +0530

    firebase configured

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -14,7 +14,7 @@ const Layout = () => {
             {isLoggedIn ? (
                 <div className="flex flex-col md:flex-row min-h-screen">
                     <Sidebar />
-                    <main className="px-6 py-4">
+                    <main className="px-6 py-4 md:py-6">
                         <Outlet />
                     </main>
                 </div>
